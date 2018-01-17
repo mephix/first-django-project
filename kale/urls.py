dfbc7fbf89804bf8c5086f3bd999aaf2f08f4e72
John Prins <john.prins@gmail.com>
2018-01-16 20:12:23 -0800

    undeserved broccoli tastes best

--- kale/urls.py
+++ kale/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('whatclass',views.whatclass,name='whatclass'),
+    path('when',views.when,name='when'),
+    path('howmuch',views.howmuch,name='howmuch'),
+    path('travel',views.travel,name='travel'),
+    path('okcool',views.okcool,name='okcool'),
+    
     ]
--- kale/views.py
+++ kale/views.py
@@ -3,4 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    # ... do any python stuff ...
+    
+    # all Django wants finally is this HttpResponse
     return HttpResponse('welcome to Kal, an app for scheduling classes and events')
+
+def whatclass(request):
+    eventchoices = Event.category.choices
+    
+    return HttpResponse(eventchoices)
+    
+#    return HttpResponse('What class do you want to take?')
+    
+def when(request):
+    return HttpResponse('What days and times are you free?')
+    
+def howmuch(request):
+    return HttpResponse('Whats the maximum you are willing to pay for a class?')
+    
+def travel(request):
+    return HttpResponse('travel preferences coming soon')
+    
+def okcool(request):
+    return HttpResponse('ok cool, we will find a class for you')
